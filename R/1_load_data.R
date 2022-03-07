9f01ae69a916cfe3cf23e24adf4b011ad15e6155
Irene de la Torre <irene.delatorre@gmail.com>
2022-03-07 23:16:55 +0100

    First merge
    Included R scripts to match the different datasets.

---
+++ R/1_load_data.R
@@ -0,0 +1,65 @@
+library(readr)
+library(readxl)
+library(dplyr)
+
+source <- "../network_data/"
+
+# Matrix file ------------------------------------------------------------------
+
+f1_Louvre_network <- read_csv(
+  paste0(source, "Rooms.Matrix/Louvre_network.csv")
+  )
+
+# Room data --------------------------------------------------------------------
+f2_Louvre_network_Wi_Fi_miyajima_200528fin <- read_excel(
+  paste0(
+    source,
+    "Rooms.Access.Points/Louvre_network+Wi-Fi_miyajima_200528fin.xlsx"
+    )
+  ) %>%
+  rename(id_ap_1 = "ID_Access_Point")
+
+f3_WiFi_rename_miyajima_200528fin <- read_excel(
+  paste0(
+    source,
+    "Rooms.Access.Points/Wi-Fi_rename_miyajima_200528fin.xlsx"
+    )
+  ) %>%
+  rename(id_ap_1 = "No.") %>%
+  select(c(id_ap_1, memo, mac))
+
+f4_WIFI_LOUVRE_TRUE <- read_excel(
+  paste0(source, "Rooms.Access.Points/WIFI_LOUVRE_TRUE.xlsx")
+) %>%
+  rename(
+    id_ap_2 = Borne,
+    mac_eth = "MAC ETH",
+    mac_radio = "range MAC Radio", 
+    location = "Emplacement") %>%
+  select(c(id_ap_2, mac_eth, mac_radio, location))
+
+# user data --------------------------------------------------------------------
+
+f5A_Louvre_MIT_1_2017_11 <- read_csv(
+  paste0(source,"Users.Locations/Louvre_MIT(11-2017)_v1.csv"))
+
+f5A_Louvre_MIT_1_2017_12 <- read_csv(
+  paste0(source,"Users.Locations/Louvre_MIT(12-2017)_v2.csv"))
+
+f5A_Louvre_MIT_1_2018 <- read_csv(
+  paste0(source,"Users.Locations/Louvre_MIT(1-2018)_v3.csv"))
+
+f5_Louvre_MIT <- rbind(
+  f5A_Louvre_MIT_1_2018,
+  f5A_Louvre_MIT_1_2017_11,
+  f5A_Louvre_MIT_1_2017_12) %>%
+  rename(
+    user_id = ID,
+    id_ap_2 = ACCESS_POINT_HOSTNAME,
+    ip_address = ACCESS_POINT_IP_ADDRESS,
+    date = DATE,
+    start = TIME_DEB,
+    end = TIME_END,
+    duration = TIME_DUR,
+    weekday = WEEKDAY
+  )
